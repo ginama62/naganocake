d49a97daab757aaa60791523934430085744fbfd
ginama62 <107463338+ginama62@users.noreply.github.com>
2022-10-23 11:53:37 +0000

    会員詳細画面、会員情報編集画面、マイページ、登録情報編集画面を作成

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,43 @@
+class Public::CustomersController < ApplicationController
+  
+  before_action :authenticate_customer!
+
+  def show
+    @customer = current_customer
+    @customers = Customer.all
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報の編集が完了しました"
+      redirect_to public_customers_my_page_path(current_customer)
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+    @customer = current_customer
+    
+  def withdraw
+     @customer = current_customer
+     @customer.update(is_deleted: true)
+     reset_session
+     flash[:notice] = "退会処理を実行いたしました"
+     redirect_to root_path
+  end
+  end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number,:email, :is_deleted)
+  end
+
+end

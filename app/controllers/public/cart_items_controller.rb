2db5ad897049487ef324256daa98bd5ba7682550
ginama62 <107463338+ginama62@users.noreply.github.com>
2022-10-30 09:47:07 +0000

    カート画面作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -26,18 +26,31 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = current_customer.cart_items.build(cart_item_params)
-    @cart_items = current_customer.cart_items.all
-    @cart_items.each do |cart_item|
-      if cart_item.item_id == @cart_item.item_id
-        new_amount = cart_item.amount + @cart_item.amount.to_i
-        cart_item.update_attribute(:amount, new_amount)
-        @cart_item.delete
-      end
+    @cart_item=CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if current_customer.cart_items.find_by(item_id: cart_item_params[:item_id]).present?
+      cart_item=current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+      cart_item.amount = cart_item.amount + cart_item_params[:amount].to_i
+      cart_item.save
+      redirect_to public_cart_items_path
+    else
+      @cart_item.save
+      redirect_to public_cart_items_path
     end
-    @cart_item.save
-    redirect_to public_cart_items_path
   end
+  #   @cart_item = CartItem.new(cart_item_params)
+  #   if CartItem.find_by(item_id: @cart_item.item_id)
+  #     @cart_item_past = CartItem.find_by(item_id: @cart_item.item_id)
+  #     @cart_item.amount +=  @cart_item_past.amount
+  #     @cart_item.save
+  #     @cart_item_past.destroy
+  #   else
+  #     @cart_item = CartItem.new(cart_item_params)
+  #   end
+  #     @cart_item.customer_id = current_customer.id
+  #     @cart_item.save
+  #     redirect_to public_cart_items_path
+  # end
 
   private
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,8 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :order
+  belongs_to :item
+
+  def subtotal #小計
+    item.with_tax_price * amount
+  end
 end
